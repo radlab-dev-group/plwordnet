6ae873d9b24870d87c4ac3bea88e9206ef0d553b
Paweł Kędzia <pawel@pradlab.dev>
2025-08-11 01:50:08 +0200

    Added prev files --- TO REFACTOR!

---
+++ plwordnet_trainer/embedding/embedding/run_train_biencoder_eurobert_0.61b.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export CUDA_VISIBLE_DEVICES=2
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+
+OUT_DIR="/mnt/local/plwn-semantic-embeddings"
+BASE_MODEL="/mnt/data2/llms/models/community/EuroBERT/EuroBERT-610m"
+BASE_NAME=$(basename "${BASE_MODEL}")
+
+#DATASET_SUBDIR="test_dataset_ratio-2.0"
+DATASET_SUBDIR="full_dataset_ratio-2.0_train0.9_eval0.1"
+DATASET_PATH="../../_resources_local/datasets/embedder/${DATASET_SUBDIR}"
+
+DATASET_TRAIN="${DATASET_PATH}/train.json"
+DATASET_EVAL="${DATASET_PATH}/test.json"
+
+DATE_AS_STR=$(date +%Y%m%d_%H%M%S)
+python3 train-bi-encoder.py \
+  -m "${BASE_MODEL}" \
+  --train-file "${DATASET_TRAIN}" \
+  --valid-file "${DATASET_EVAL}" \
+  -O "${OUT_DIR}/${BASE_NAME}/biencoder/${DATE_AS_STR}_${DATASET_SUBDIR}" \
+  -WB

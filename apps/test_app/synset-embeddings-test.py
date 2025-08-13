f07ebc161dca9aa7bb57b65503942dbccdc2c8c1
Paweł Kędzia <pawel@radlab.dev>
2025-08-14 00:38:49 +0200

    Updated sample (synset/lu) embedding generator

--- apps/test_app/synset-embeddings-test.py
+++ apps/test_app/synset-embeddings-test.py
@@ -1,44 +1,66 @@
-import sys
-import logging
-
 from plwordnet_handler.base.structure.polishwordnet import PolishWordnet
-from plwordnet_handler.base.connectors.nx.nx_connector import PlWordnetAPINxConnector
-from plwordnet_trainer.embedder.synset.generator import (
-    EmbeddingGenerator,
-    SynsetEmbeddingGenerator,
+from plwordnet_handler.utils.logger import prepare_logger
+from plwordnet_handler.base.connectors.milvus.core.schema import (
+    MilvusWordNetSchemaHandler,
+)
+from plwordnet_trainer.embedder.generator.lexical_unit import (
+    BiEncoderEmbeddingGenerator,
+    LexicalUnitEmbeddingGenerator,
+)
+from plwordnet_handler.base.connectors.milvus.milvus_connector import MilvusConnector
+from plwordnet_trainer.embedder.generator.strategy import EmbeddingBuildStrategy
+
+MILVUS_CONFIG = (
+    "/mnt/data2/dev/develop/radlab-plwordnet/resources/milvus-config.json"
+)
+NX_GRAPHS = (
+    "/mnt/data2/data/resources/plwordnet_handler/20250811/slowosiec_test/nx/graphs"
+)
+
+CREATE_SCHEMA = False
+
+LOG_LEVEL = "INFO"
+LOG_FILENAME = "synset-embeddings.log"
+
+logger = prepare_logger(
+    logger_name=__name__,
+    logger_file_name=LOG_FILENAME,
+    log_level=LOG_LEVEL,
+    use_default_config=True,
 )
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[
-        logging.StreamHandler(sys.stdout),
-        logging.FileHandler("plwordnet_trainer.log"),
-    ],
-)
+if CREATE_SCHEMA:
+    handler = MilvusWordNetSchemaHandler.from_config_file(config_path=MILVUS_CONFIG)
+    handler.initialize()
 
-logger = logging.getLogger(__name__)
+    handler.connect()
+    print(handler.get_status())
+    handler.disconnect()
+else:
+    print("???????????????")
+    milvus = MilvusConnector.from_config_file(
+        config_path=MILVUS_CONFIG,
+    )
 
+    with PolishWordnet(nx_graph_dir=NX_GRAPHS, use_memory_cache=True) as pl_wn:
+        logger.info("PolishWordnet connected")
 
-generator = EmbeddingGenerator(
-    model_path="/mnt/data2/llms/models/radlab-open/embedders/radlab_polish-bi-encoder-mean",
-    device="cuda:0",
-)
+        syn_emb_generator = LexicalUnitEmbeddingGenerator(
+            generator=BiEncoderEmbeddingGenerator(
+                model_path="/mnt/data2/llms/models/radlab-open/embedders/radlab_polish-bi-encoder-mean",
+                device="cpu",
+                normalize_embeddings=True,
+                log_level=LOG_LEVEL,
+                log_filename=LOG_FILENAME,
+            ),
+            pl_wordnet=pl_wn,
+            log_level=LOG_LEVEL,
+            log_filename=LOG_FILENAME,
+            spacy_model_name="pl_core_news_sm",
+            strategy=EmbeddingBuildStrategy.MEAN,
+        )
+        logger.info("SynsetEmbeddingGenerator created")
 
-connector = PlWordnetAPINxConnector(
-    nx_graph_dir="/mnt/data2/data/resources/plwordnet_handler/20250811/slowosiec_test/nx/graphs",
-    autoconnect=True,
-)
-pl_wn = PolishWordnet(
-    connector=connector,
-    db_config_path=None,
-    nx_graph_dir=None,
-    extract_wiki_articles=False,
-    use_memory_cache=True,
-    show_progress_bar=False,
-)
-
-syn_emb_generator = SynsetEmbeddingGenerator(generator=generator, pl_wordnet=pl_wn)
-
-syn_emb_generator.run(batch_size=1024)
+        for e_dict in syn_emb_generator.generate(split_to_sentences=True):
+            pass

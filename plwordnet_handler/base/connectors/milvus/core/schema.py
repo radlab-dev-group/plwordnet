85504a0cd1b2d161b7cabdb8fc3a763c756c1fdd
Paweł Kędzia <pawel@pradlab.dev>
2025-08-14 03:22:41 +0200

    Added model name to schema + new collection/schema for lu examples

--- plwordnet_handler/base/connectors/milvus/core/schema.py
+++ plwordnet_handler/base/connectors/milvus/core/schema.py
@@ -57,11 +57,62 @@ class _SchemaDef:
                     dtype=DataType.INT32,
                     description="Variant information",
                 ),
+                FieldSchema(
+                    name="model_name",
+                    dtype=DataType.VARCHAR,
+                    max_length=500,
+                    description="Name of model used to generate embeddings",
+                ),
             ]
 
             schema = CollectionSchema(
                 fields=fields,
                 description="Lexical unit embeddings with metadata",
+                enable_dynamic_field=True,
+            )
+
+            return schema
+
+    class LUExample:
+        @classmethod
+        def create_schema(cls, emb_size: int) -> CollectionSchema:
+            """
+            Create a schema for lexical unit examples embeddings' collection.
+
+            Returns:
+                CollectionSchema: Schema for lexical unit examples embeddings
+            """
+            fields = [
+                FieldSchema(
+                    name="id",
+                    dtype=DataType.INT64,
+                    is_primary=True,
+                    auto_id=False,
+                    description="Lexical Unit ID from WordNet",
+                ),
+                FieldSchema(
+                    name="embedding",
+                    dtype=DataType.FLOAT_VECTOR,
+                    dim=emb_size,
+                    description="Lexical unit example embedding vector",
+                ),
+                FieldSchema(
+                    name="example",
+                    dtype=DataType.VARCHAR,
+                    max_length=1024,
+                    description="lexical unit example (definition, sentiment, etc.)",
+                ),
+                FieldSchema(
+                    name="model_name",
+                    dtype=DataType.VARCHAR,
+                    max_length=500,
+                    description="Name of model used to generate embeddings",
+                ),
+            ]
+
+            schema = CollectionSchema(
+                fields=fields,
+                description="Lexical unit examples embeddings with metadata",
                 enable_dynamic_field=True,
             )
 
@@ -96,6 +147,12 @@ class _SchemaDef:
                     max_length=500,
                     description="String representation of units in synset",
                 ),
+                FieldSchema(
+                    name="model_name",
+                    dtype=DataType.VARCHAR,
+                    max_length=500,
+                    description="Name of model used to generate embeddings",
+                ),
             ]
 
             schema = CollectionSchema(
@@ -123,6 +180,7 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
         config: Optional[MilvusConfig] = None,
         syn_vector_dim: int = 1024,
         lu_vector_dim: int = 1024,
+        lu_text_vector_dim: int = 1024,
         log_level: str = "INFO",
     ):
         """
@@ -138,8 +196,10 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
             config: Optional MilvusConfig object for advanced configuration
             syn_vector_dim: Dimension size for synset embedding vectors.
             Defaults to 1024
-            lu_vector_dim: Dimension size for "lexical unit embedding" vectors.
-            Defaults to 1024
+            lu_vector_dim: Dimension size for "lexical unit embedding"
+            vectors. Defaults to 1024
+            lu_text_vector_dim: Dimension size for a vector of texts
+            belonging to lexical units. Defaults to 1024
             log_level: Logging level for the connector. Defaults to "INFO"
         """
 
@@ -161,6 +221,10 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
         self.lu_schema = None
         self.lu_collection_name = "lu_embeddings"
         self.lu_vector_dim = lu_vector_dim
+
+        self.lu_examples_schema = None
+        self.lu_examples_collection_name = "lu_texts_embeddings"
+        self.lu_example_vector_dim = lu_vector_dim
 
     def get_status(self) -> Dict[str, Any]:
         """
@@ -187,6 +251,7 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
         try:
             self.__create_synset_collection()
             self.__create_lexical_unit_collection()
+            self.__create_lexical_unit_examples_collection()
             return True
         except MilvusException as e:
             self.logger.error(f"Failed to create collections: {e}")
@@ -225,7 +290,15 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
                 field_name="embedding", index_params=index_params
             )
 
-            self.logger.info(f"Created {index_type} indexes on both collections")
+            # Create an index for LU examples collection
+            lu_examples_collection = Collection(
+                self.lu_examples_collection_name, using=self.conn_name
+            )
+            lu_examples_collection.create_index(
+                field_name="embedding", index_params=index_params
+            )
+
+            self.logger.info(f"Created {index_type} indexes on collections")
             return True
         except MilvusException as e:
             self.logger.error(f"Failed to create indexes: {e}")
@@ -252,6 +325,18 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
         """
         self.lu_schema = _SchemaDef.LU.create_schema(emb_size=self.lu_vector_dim)
         return self.lu_schema
+
+    def __create_lu_examples_schema(self) -> CollectionSchema:
+        """
+        Create a schema for lexical unit examples embeddings' collection.
+
+        Returns:
+            CollectionSchema: Schema for lexical unit examples embeddings
+        """
+        self.lu_examples_schema = _SchemaDef.LUExample.create_schema(
+            emb_size=self.lu_example_vector_dim
+        )
+        return self.lu_examples_schema
 
     def __create_synset_collection(self):
         """
@@ -294,3 +379,29 @@ class MilvusWordNetSchemaHandler(MilvusWordNetBaseHandler):
             self.logger.info(f"Created collection: {self.lu_collection_name}")
         else:
             self.logger.info(f"Collection {self.lu_collection_name} already exists")
+
+    def __create_lexical_unit_examples_collection(self):
+        """
+        Create a Milvus collection for lexical unit examples
+        embeddings if it doesn't exist.
+
+        Checks if the lexical unit examples collection already exists and creates
+        it with the appropriate schema if not found. Logs the creation status.
+        """
+
+        if not utility.has_collection(
+            self.lu_examples_collection_name, using=self.conn_name
+        ):
+            lu_examples_schema = self.__create_lu_examples_schema()
+            Collection(
+                name=self.lu_examples_collection_name,
+                schema=lu_examples_schema,
+                using=self.conn_name,
+            )
+            self.logger.info(
+                f"Created collection: {self.lu_examples_collection_name}"
+            )
+        else:
+            self.logger.info(
+                f"Collection {self.lu_examples_collection_name} already exists"
+            )

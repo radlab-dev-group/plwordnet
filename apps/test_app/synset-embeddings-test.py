79b06089b43b18c05439f5fffb5fc2b6eab00025
Paweł Kędzia <pawel@radlab.dev>
2025-08-14 03:24:55 +0200

    Integration with new modules

--- apps/test_app/synset-embeddings-test.py
+++ apps/test_app/synset-embeddings-test.py
@@ -3,7 +3,9 @@ from plwordnet_handler.utils.logger import prepare_logger
 from plwordnet_handler.base.connectors.milvus.core.schema import (
     MilvusWordNetSchemaHandler,
 )
-from plwordnet_handler.base.connectors.milvus.milvus_connector import MilvusConnector
+from plwordnet_handler.base.connectors.milvus.core.io import (
+    MilvusWordNetInsertHandler,
+)
 
 from plwordnet_handler.base.structure.polishwordnet import PolishWordnet
 from plwordnet_trainer.embedder.generator.strategy import EmbeddingBuildStrategy
@@ -13,18 +15,30 @@ from plwordnet_trainer.embedder.generator.bi_encoder import (
 from plwordnet_trainer.embedder.generator.lexical_unit import (
     LexicalUnitEmbeddingGenerator,
 )
+from plwordnet_trainer.embedder.milvus.consumer import EmbeddingMilvusConsumer
+
+
+DEVICE = "cuda:0"
+LOG_LEVEL = "INFO"
+CREATE_SCHEMA = True
 
 MILVUS_CONFIG = (
     "/mnt/data2/dev/develop/radlab-plwordnet/resources/milvus-config-pk.json"
 )
 NX_GRAPHS = (
-    "/mnt/data2/data/resources/plwordnet_handler/20250811/slowosiec_test/nx/graphs"
+    "/mnt/data2/data/resources/plwordnet_handler/20250811/slowosiec_full/nx/graphs"
 )
 
-CREATE_SCHEMA = False
+# Bi-encoder model spec
+BI_ENCODER_MODEL_NAME = ""
+BI_ENCODER_WANDB = (
+    "http://192.168.100.61:8080/pkedzia/plWordnet-semantic-embeddings/runs/o7reqebo"
+)
+BI_ENCODER_MODEL_PATH = "/mnt/data2/llms/models/radlab-open/embedders/plwn-semantic-embeddingss/v0.1/EuroBERT-610m/biencoder/20250806_162431_full_dataset_ratio-2.0_train0.9_eval0.1/checkpoint-290268"
 
-LOG_LEVEL = "INFO"
+SPACY_MODEL_NAME = "pl_core_news_sm"
 LOG_FILENAME = "synset-embeddings.log"
+
 
 logger = prepare_logger(
     logger_name=__name__,
@@ -39,12 +53,13 @@ if CREATE_SCHEMA:
     handler.initialize()
 
     handler.connect()
-    print(handler.get_status())
+    logger.info(handler.get_status())
     handler.disconnect()
 else:
-    print("???????????????")
-    milvus = MilvusConnector.from_config_file(
-        config_path=MILVUS_CONFIG,
+    embedding_consumer = EmbeddingMilvusConsumer(
+        milvus=MilvusWordNetInsertHandler.from_config_file(
+            config_path=MILVUS_CONFIG,
+        )
     )
 
     with PolishWordnet(nx_graph_dir=NX_GRAPHS, use_memory_cache=True) as pl_wn:
@@ -52,8 +67,9 @@ else:
 
         syn_emb_generator = LexicalUnitEmbeddingGenerator(
             generator=BiEncoderEmbeddingGenerator(
-                model_path="/mnt/data2/llms/models/radlab-open/embedders/radlab_polish-bi-encoder-mean",
-                device="cpu",
+                model_path=BI_ENCODER_MODEL_PATH,
+                model_name=BI_ENCODER_MODEL_NAME,
+                device=DEVICE,
                 normalize_embeddings=True,
                 log_level=LOG_LEVEL,
                 log_filename=LOG_FILENAME,
@@ -61,10 +77,11 @@ else:
             pl_wordnet=pl_wn,
             log_level=LOG_LEVEL,
             log_filename=LOG_FILENAME,
-            spacy_model_name="pl_core_news_sm",
+            spacy_model_name=SPACY_MODEL_NAME,
             strategy=EmbeddingBuildStrategy.MEAN,
+            max_workers=4,
         )
         logger.info("SynsetEmbeddingGenerator created")
 
         for e_dict in syn_emb_generator.generate(split_to_sentences=True):
-            pass
+            embedding_consumer.add_embedding(embedding_dict=e_dict)

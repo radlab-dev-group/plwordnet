b22e746574dbaec40f5e559eae4dc3580b01611b
Paweł Kędzia <pawel@radlab.dev>
2025-08-16 14:47:51 +0200

    Added example usages of plwordnet-milvus CLI

--- plwordnet_trainer/cli/example_usage.py
+++ plwordnet_trainer/cli/example_usage.py
@@ -4,5 +4,18 @@ Example usage documentation for the Polish Wordnet Milvus CLI application.
 
 EXAMPLE_USAGE = f"""
 Example usage:
-python plwordnet-milvus
+
+# Help
+python plwordnet-milvus --help
+
+# Initialize database(database, schemas, indexes, collections)
+plwordnet-milvus 
+    --milvus-config=resources/milvus-config.json 
+    --prepare-database
+
+# Prepare base embeddings (this step have to be done before embeddings fusion)
+plwordnet-milvus 
+    --milvus-config=resources/milvus-config.json 
+    --prepare-base-embeddings 
+    --device="cuda:1"
 """

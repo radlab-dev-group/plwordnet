f7b0e040a8cd270f3ab7a76c1e73b1e6a408947d
Paweł Kędzia <pawel@radlab.dev>
2025-09-19 16:50:43 +0200

    --workers-count is set to the number which does not exceed the wiki limit for today.

--- scripts/1-plwordnet-cli-dump-to-nx.sh
+++ scripts/1-plwordnet-cli-dump-to-nx.sh
@@ -14,7 +14,7 @@ plwordnet-cli \
   --nx-graph-dir="${NX_GRAPH_DIR}" \
   --convert-to-nx-graph \
   --extract-wikipedia-articles \
-  --workers-count=32 \
+  --workers-count=10 \
   --correct-texts \
   --prompts-dir=resources/prompts \
   --prompt-name="ollama/correct_text" \

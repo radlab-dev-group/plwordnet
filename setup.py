8255bb8eff2fbd78b515785bb7c7608010375be2
Paweł Kędzia <pawel@radlab.dev>
2025-09-21 14:45:43 +0200

    Installing system cli (plwordnet + milvus)

--- setup.py
+++ setup.py
@@ -6,5 +6,11 @@ setup(
     version="0.0.1",
     packages=find_packages(exclude=("tests", "docs")),
     python_requires=">=3.10",
+    entry_points={
+        "console_scripts": [
+            "plwordnet-cli=apps.cli.plwordnet_cli:main",
+            "plwordnet-milvus=apps.cli.plwordnet_milvus_cli:main",
+        ],
+    },
     install_requires=open("requirements.txt").read().splitlines(),
 )

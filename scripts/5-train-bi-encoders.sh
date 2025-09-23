1e6f821285372296e6c0a14d778204c05ed3e1ff
Paweł Kędzia <pawel@radlab.dev>
2025-09-23 19:59:06 +0200

    Install updates -> add 5-train-bi-encoders.sh bash script.

---
+++ scripts/5-train-bi-encoders.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Use scripts from plwordnet_ml/training_scripts"
+echo "  - run_train_biencoder_eurobert_0.61b.sh  - to Train EuroBERT in 610m architecture"
+echo "  - run_train_biencoder_eurobert_2.1b.sh   - to Train EuroBERT in 2.1b architecture"

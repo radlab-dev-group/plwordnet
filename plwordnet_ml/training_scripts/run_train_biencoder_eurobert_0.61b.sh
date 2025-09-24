75be162601c2c35fb6b29cb9395389a9701d6b42
Paweł Kędzia <pawel@radlab.dev>
2025-09-24 11:46:21 +0200

    New dataset in run_train_biencoder_eurobert_0.61b.sh

--- plwordnet_ml/training_scripts/run_train_biencoder_eurobert_0.61b.sh
+++ plwordnet_ml/training_scripts/run_train_biencoder_eurobert_0.61b.sh
@@ -4,12 +4,12 @@ export CUDA_VISIBLE_DEVICES=2
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
-OUT_DIR="/mnt/local/plwn-semantic-embeddings"
+OUT_DIR="/mnt/local/plwn-semantic-embeddings-pl-en"
 BASE_MODEL="/mnt/data2/llms/models/community/EuroBERT/EuroBERT-610m"
 BASE_NAME=$(basename "${BASE_MODEL}")
 
-DATASET_SUBDIR="embedder_sentsplit_train-0.93"
-DATASET_PATH="/mnt/data2/data/datasets/radlab-semantic-embeddings/20250811/embedding-dump-ratio-1.2-w-synonymy/${DATASET_SUBDIR}"
+DATASET_SUBDIR="20250924/plwn_4_5_embedder_dataset/"
+DATASET_PATH="/mnt/data2/data/datasets/radlab-semantic-embeddings/${DATASET_SUBDIR}"
 
 DATASET_TRAIN="${DATASET_PATH}/train_deduplicated.json"
 DATASET_EVAL="${DATASET_PATH}/test_deduplicated.json"

ed12d17e57d3de2fe87570f46ef6242ff50eac83
Paweł Kędzia <pawel@radlab.dev>
2025-11-02 11:08:54 +0100

    Add `--embedder-config` argument to `7-plwordnet-milvus-relgat-export.sh` to specify embedder configuration.

--- scripts/7-plwordnet-milvus-relgat-export.sh
+++ scripts/7-plwordnet-milvus-relgat-export.sh
@@ -2,6 +2,7 @@
 
 plwordnet-milvus \
   --milvus-config=resources/configs/milvus-config-pk.json \
+  --embedder-config=resources/configs/embedder-config.json \
   --nx-graph-dir="resources/plwordnet_4_5/full/graphs/full/nx/graphs/" \
   --relgat-mapping-directory="resources/plwordnet_4_5/full/relgat/aligned-dataset-identifiers/o78zalgm" \
   --relgat-dataset-directory="resources/plwordnet_4_5/full/relgat/aligned-dataset-identifiers/o78zalgm/dataset_syn_two_way" \

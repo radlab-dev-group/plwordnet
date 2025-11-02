96a429f653213938752e3c3a384a821dffb481a5
Paweł Kędzia <pawel@radlab.dev>
2025-11-02 14:26:44 +0100

    Parameterize config paths, CUDA device, and workers count in the full‑init script and pass `--workers-count` to the base embeddings step.

--- scripts/6-plwordnet-milvus-full-init.sh
+++ scripts/6-plwordnet-milvus-full-init.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-# prepare database
+# Workers count
+WORKERS_COUNT=2
+
+# Which device will be used to prepare embeddings
+CUDA_DEVICE="cuda:1"
+
+# path to Milvus database configuration
+MILVUS_CONFIG="../resources/configs/milvus-config.json"
+
+# path to embedder configuration
+EMBEDDER_CONFIG="../resources/configs/embedder-config.json"
+
+
+# ##################################################################################
+# Prepare database
+# ##################################################################################
 plwordnet-milvus \
   --log-level=DEBUG \
-  --milvus-config=../resources/configs/milvus-config.json \
+  --milvus-config="${MILVUS_CONFIG}"on \
   --prepare-database
 
 
+# ##################################################################################
 # Base and fake embeddings
+# ##################################################################################
 plwordnet-milvus \
-  --milvus-config=../resources/configs/milvus-config.json \
-  --embedder-config=../resources/configs/embedder-config.json \
+  --milvus-config="${MILVUS_CONFIG}" \
+  --embedder-config="${EMBEDDER_CONFIG}" \
   --nx-graph-dir=../resources/plwordnet_4_5/full/graphs/full/nx/graphs/ \
-  --device="cuda:1" \
+  --device="${CUDA_DEVICE}" \
   --log-level=INFO \
+  --workers-count=${WORKERS_COUNT} \
   --prepare-base-embeddings-lu \
   --prepare-base-embeddings-synset \
   --prepare-base-mean-empty-embeddings-lu
